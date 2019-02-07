10cf5a0da4239487fe383538d79684d0a66398f2
Shamanth <45139850+shamanthsamadeshi@users.noreply.github.com>
2019-02-08 03:11:09 +0530

    Update .IP-Tracer.php

--- .IP-Tracer.php
+++ .IP-Tracer.php
@@ -10,10 +10,11 @@ function logo() {
 
 
 \033[01;31m      _\033[01;33m ____    _
-     \033[01;31m(_)\033[01;33m  _ \  | |_ _ __ __ _  ___ ___ _ __
-     | | |_) | | __| '__/ _` |/ __/ _ \ '__|
-     | |  __/  | |_| | | (_| | (_|  __/ |
-     |_|_|      \__|_|  \__,_|\___\___|_|
+     \033[01;31m(_)\033[01;33m 
+     | || ~~~~|      | |_   _ __.   __ _  ___   ___   _ __.
+     | || |_) |  ==  | __| | '__| / _` | / __| / _ \ | '__|
+     | ||  __/   ==  | |_| | |   | (_| || (_  | ___/ | |
+     |_||_|           \__| |_|    \__,_| \___| \___| | |
 
 
    \033[01;37m}\033[01;31m----------------------------------------\033[01;37m{
@@ -29,9 +30,8 @@ function About() {
   echo <<<EOL
 
          \033[01;33mTool Name \033[01;37m:- \033[01;36mIP-Tracer
-         \033[01;33mAuthor \033[01;37m:- \033[01;36mRajkumar Dusad
-         \033[01;33mPowered By \033[01;37m:- \033[01;36mAex Software's
-
+         \033[01;33mAuthor \033[01;37m:- \033[01;36mShamanth Samadeshi
+         
  \033[01;33mIP-Tracer\033[01;32m is use \033[01;36mip-api \033[01;32mto retrive any ip address information. Our system will automatically ban any IP addresses doing over 150 requests per minute.\033[00m
 
 
@@ -44,8 +44,8 @@ function upd() {
   logo();
   echo "\n\033[01;32mUpdating IP-Tracer.........\033[01;37m\n\n";
   sleep(1);
-  system("cd ~/ && git clone https://github.com/Rajkumrdusad/IP-Tracer.git");
-  system("cd ~/ && sudo git clone https://github.com/Rajkumrdusad/IP-Tracer.git");
+  system("cd ~/ && git clone https://github.com/shamanthsamadeshi/iptracker.git");
+  system("cd ~/ && sudo git clone https://github.com/shamanthsamadeshi/iptracker.git");
   system("cd ~/IP-Tracer && sh install");
   logo();
   echo "\n\033[01;32m              IP-Tracer updated !!!\033[01;37m\n";
